29e52f0c97703b5adc91bd628d399ed21d94f937
stepfunc-droid <stepfunc@gmail.com>
2026-09-05 00:22:58 +0800

    suez: trim unused system apps

--- device.mk
+++ device.mk
@@ -65,7 +65,7 @@ PRODUCT_COPY_FILES += \
     $(DEVICE)/audio/audio_policy.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy.conf
 
 PRODUCT_COPY_FILES += \
-    $(TOPDIR)frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_audio_policy_configuration.xml \
+    $(TOPDIR)frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
     $(TOPDIR)frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
     $(TOPDIR)frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
     $(TOPDIR)frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
@@ -177,3 +177,23 @@ $(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-2048-hwui
 
 # Get non-open-source specific aspects
 $(call inherit-product-if-exists, $(VENDOR)/suez-vendor.mk)
+
+# Keep the system image lean. Suez is Wi-Fi only, and these apps are either
+# replaceable from Play Store or unnecessary for this build.
+SUEZ_REMOVE_PACKAGES := \
+    AudioFX \
+    Backgrounds \
+    Contacts \
+    DeskClock \
+    Email \
+    Eleven \
+    Gallery2 \
+    LatinIME \
+    LineageSetupWizard \
+    Recorder \
+    Telecom \
+    TeleService \
+    Traceur \
+    Updater
+
+PRODUCT_PACKAGES := $(filter-out $(SUEZ_REMOVE_PACKAGES),$(PRODUCT_PACKAGES))
